d27e0fa5a3f194d419e6eae34ce6c3bf2d8f34a3
CaporaliGiorgio <giorgio.caporali.1@gmail.com>
2021-12-07 09:52:42 +0100

    seconda fASE

--- app/src/main/java/itts/volta/quintocinf/array5d/MainActivity.java
+++ app/src/main/java/itts/volta/quintocinf/array5d/MainActivity.java
@@ -3,12 +3,22 @@ package itts.volta.quintocinf.array5d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nazioni[]= {"Italia","Francia","Germania","Olanda" };
+    ListView listaStati;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaStati = (ListView)findViewById(R.id.lvStati);
+        ArrayAdapter<String> aaStati = new ArrayAdapter<String>(this,R.layout.activity_main,nazioni);
+        listaStati.setAdapter(aaStati);
     }
 }
